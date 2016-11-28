5eba9931d14e9461ff12d4b95647f28c049a3315
Minh Nguyen Tien <TooNies1810@minhs-mbp.dsldevice.lan>
2016-11-29 00:07:53 +0700

    ignore

--- index.js
+++ index.js
@@ -41,5 +41,5 @@ app.post('/hong', urlencodedParser, function (req, res) {
 });
 
 app.listen(3001, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('listening on port 3000!');
 });
