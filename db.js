728f8fcab9170a0f7dcc917768767d02c6bf50db
minhnt58 <tienminh.uet@gmail.com>
2016-12-01 21:06:19 +0700

    refactor

--- db.js
+++ db.js
@@ -2,9 +2,6 @@
 
 let Datastore = require('nedb');
 
-// var db = {};
-// db.users = ;
-
 module.exports = function () {
     return {
         users: new Datastore({filename: 'databases/users.db', autoload: true})
